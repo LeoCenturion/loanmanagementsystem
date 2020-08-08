28006ccdfed76277ffafcaad32e79f440a20fe17
LeoCenturion <centurionm.leo@gmail.com>
2020-08-08 17:55:52 -0300

    Rename application

--- src/main/java/com/cashonline/loanmanagementsystem/LoanManagementSystemApplication.java
+++ src/main/java/com/cashonline/loanmanagementsystem/LoanManagementSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoanmanagementsystemApplication {
+public class LoanManagementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoanmanagementsystemApplication.class, args);
+		SpringApplication.run(LoanManagementSystemApplication.class, args);
 	}
 
 }
